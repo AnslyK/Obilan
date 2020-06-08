a9153f60dbe346f369571ce8a5cb81d753bb4320
unknown <Kaotico06@gmail.com>
2020-06-08 23:32:11 +0200

    (back) ajout d'un fichier de config

---
+++ back-end/admin/config.js
@@ -0,0 +1,9 @@
+const _BDD_HOST_ = "localhost";
+const _BDD_DATABASE_ = "obilan";
+const _BDD_USER_ = "root"
+const _BDD_PASSWORD_ = "";
+
+exports._BDD_DATABASE_ = _BDD_DATABASE_;
+exports._BDD_HOST_ = _BDD_HOST_;
+exports._BDD_USER_ = _BDD_USER_;
+exports._BDD_PASSWORD_ = _BDD_PASSWORD_;
