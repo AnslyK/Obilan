694b05f5913562c28d3d50c53197d28b83b03804
unknown <Kaotico06@gmail.com>
2020-06-10 19:07:26 +0200

    (back) rename lib, and adding connection + signin

--- back-end/controllers/account/lib.js
+++ back-end/controllers/account/lib.js
@@ -1,19 +1,25 @@
 const passwordHash = require("password-hash");
 
 async function signup(req, res) {
-  const { password, email, name } = req.body;
-  if (!email || !password || !name) {
-    //Le cas où l'email ou bien le password ne serait pas soumit ou nul
+  const { lastName, firstName, pseudo, password, email } = req.body;
+  let { mature } = req.body;
+  if (!lastName || !firstName || !pseudo || !password || !email) {
+    //Le cas où un des champs ne serait pas soumit ou nul
     return res.status(400).json({
       text: "Requête invalide"
     });
   }
+  // Si mature n'est pas défini, on let set à 0 pour correspondre au format de la base (0 ou 1)
+  if (!mature){
+    mature = '0';
+  }
+
   // Création d'un objet user, dans lequel on hash le mot de passe
   const hashedPassword = passwordHash.generate(password)
 
   // On check en base si l'utilisateur existe déjà
-    const findUser = 'SELECT usr_mail FROM user_usr WHERE usr_mail=?'
-    con.query(findUser, [ email ], function(err, result) {
+    const findUser = 'SELECT mail, pseudo FROM player WHERE mail=? OR pseudo=?'
+    con.query(findUser, [ email, pseudo ], function(err, result) {
         if (err) throw err
         if (result.length !== 0) {
             return res.status(400).json({
@@ -21,8 +27,8 @@ async function signup(req, res) {
             });
         } else {
                 // Sauvegarde de l'utilisateur en base
-            const userData = 'INSERT INTO user_usr ( usr_name, usr_mail, usr_password) VALUES (?,?,?)'
-            con.query(userData, [name, email, hashedPassword], function(err, result) {
+            const userData = 'INSERT INTO player ( lastName, firstName, pseudo, password, mature, mail) VALUES (?,?,?,?,b?,?)'
+            con.query(userData, [lastName, firstName, pseudo, hashedPassword, mature, email], function(err, result) {
                 if(err) throw err
                 return res.status(200).json({
                     text: "Succès",
@@ -44,21 +50,19 @@ async function login(req, res) {
       text: "Requête invalide"
     });
   }
-  //console.log('password = '+password+', hashedPassword = ' +hashedPassword)
+
     // On check si l'utilisateur existe en base
-    const findUser = 'SELECT usr_mail, usr_name, usr_password FROM user_usr WHERE usr_mail=?'
-    con.query(findUser, [ email ], function(err, result) {
+    const findUser = 'SELECT mail, pseudo, PASSWORD FROM player WHERE mail=? OR pseudo=?'
+    con.query(findUser, [ email, email ], function(err, result) {
         if(err) throw err
         if(result.length === 0)
         return res.status(401).json({
           text: "L'utilisateur n'existe pas"
         });
-        //console.log(result[0])
-        //console.log(result[0].usr_password)
-        if(passwordHash.verify(password, result[0].usr_password))
+        if(passwordHash.verify(password, result[0].PASSWORD))
         return res.status(200).json({
             token: passwordHash.generate(email),
-            name: result[0].usr_name,
+            name: result[0].pseudo,
             text: "Authentification réussi"
         })
 
